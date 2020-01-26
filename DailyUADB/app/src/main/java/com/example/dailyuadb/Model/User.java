183968862f72d9d94417b70e91c54649cc5cf4a2
ibrahim0495 <ibrahimdatt@gmail.com>
2020-01-26 12:07:59 +0000

    Finalisation de l'Integration inscription/connexion

--- DailyUADB/app/src/main/java/com/example/dailyuadb/Model/User.java
+++ DailyUADB/app/src/main/java/com/example/dailyuadb/Model/User.java
@@ -15,5 +15,44 @@ public class User {
         this.numCarte = numCarte;
         this.password = password;
     }
-    
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumCarte() {
+        return numCarte;
+    }
+
+    public void setNumCarte(String numCarte) {
+        this.numCarte = numCarte;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
